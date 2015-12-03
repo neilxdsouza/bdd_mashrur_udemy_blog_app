1711ef543ced9a0545a08ee3ede07130dad8f1d1
neil xavier dsouza <nxd_in@yahoo.com>
2015-12-03 08:47:49 +0530

    refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+  before_filter :set_article, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -23,15 +24,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to @article
@@ -42,7 +43,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = "Article has been deleted"
       redirect_to articles_path
@@ -54,5 +55,9 @@ class ArticlesController < ApplicationController
       def article_params
         params.require(:article).permit(:title, :body)
       end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
 end
